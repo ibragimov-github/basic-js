877244524c55eb7c12d20043173bb7fd0f620b82
ibragimov-github <texet0598@gmail.com>
2022-04-28 14:54:21 +0300

    add: new changes

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,7 +18,7 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-    if (isNaN(+sampleActivity || +sampleActivity <= 0 || typeof sampleActivity !== 'string' || sampleActivity.length === 0) || MODERN_ACTIVITY < +sampleActivity) {
+    if (isNaN(+sampleActivity || +sampleActivity <= 0 || typeof sampleActivity !== 'string' || sampleActivity.length === 0) || MODERN_ACTIVITY < +sampleActivity || +sampleActivity !== Infinity) {
         return false;
     }
     let t = Math.log(MODERN_ACTIVITY/+sampleActivity)/(0.693/HALF_LIFE_PERIOD);
