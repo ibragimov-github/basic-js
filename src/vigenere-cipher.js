aa0b2cb6833a1e4728ac69420f9e506a59d5ba76
ibragimov-github <texet0598@gmail.com>
2022-04-30 13:09:11 +0300

    add: new changes

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(value = true) {
+        this.value = value;
+    }
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    encrypt(text, key) {
+        if (!text || !key) {
+            throw Error("Incorrect arguments!");
+        }
+        let acc = 0;
+        let result = ''
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] === ' ') {
+                i++;
+                result += ' ';
+            }
+            if (this.alphabet.indexOf(text[i].toUpperCase()) !== -1) {
+                if (acc >= key.length) { acc = 0;}
+                let index = (this.alphabet.indexOf(text[i].toUpperCase()) + this.alphabet.indexOf(key[acc].toUpperCase())) % 26
+                result += this.alphabet[index];
+                acc ++;
+            }
+            else {
+                result += text[i];
+                acc ++;
+            }
+
+        }
+    if (this.value) {
+        return result;
+    }
+    return result.split('').reverse().join('');
+    }
+    decrypt(text, key) {
+        if (!text || !key) {
+            throw Error("Incorrect arguments!");
+        }
+        let acc = 0;
+        let result = '';
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] === ' ') {
+                i++;
+                result += ' ';
+            }
+            if (this.alphabet.indexOf(text[i].toUpperCase()) !== -1) {
+                if (acc >= key.length) { acc = 0;}
+                let index = this.alphabet.indexOf(text[i].toUpperCase()) - this.alphabet.indexOf(key[acc].toUpperCase());
+                if (index < 0) {
+                    index = 26 + index;
+                }
+                result += this.alphabet[index];
+                acc ++;
+            }
+            else {
+                result += text[i];
+                acc ++;
+            }
+        }
+    if (this.value) {
+        return result;
+    }
+    return result.split('').reverse().join('');
+    }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+const directMachine = new VigenereCipheringMachine(false);
+
